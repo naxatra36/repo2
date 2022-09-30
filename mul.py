4c55a5ddb2f53de43fbdad81203e0d036fcd1634
Adarsh Mohanty <102286697+20adarsh22@users.noreply.github.com>
2022-10-01 00:30:51 +0530

    Update mul.py
    Correct and Working code for multiplying two numbers in python.

--- mul.py
+++ mul.py
@@ -1,2 +1,4 @@
-a,b= map(int,input().split())
-print(a*b)
+num_1 = int(input("Enter first number = "))
+num_2 = int(input("Enter second number = "))
+product = num_1 * num_2
+print("Product of {} and {} is {}.".format(num_1, num_2,product))
